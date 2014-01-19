308b161b50d15f1f87479355ca61cffacdbfe0db
João Pinto <lamego.pinto@gmail.com>
2014-01-19 17:09:26 +0000

    Added loading screen

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 SOURCE_DIR=../farmline
 [[ ! -d $SOURCE_DIR ]] && echo "$SOURCE_DIR was not found" && exit 1
+rm -rf common
+cp -va $SOURCE_DIR/common .
 cp -vp $SOURCE_DIR/web/{*.dart,*.js} web
 cp -vp $SOURCE_DIR/packages/browser/dart.js web/packages/browser
