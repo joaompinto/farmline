f3102fbf7d0f0f91bdf997251bd14688df98c454
João Pinto <lamego.pinto@gmail.com>
2014-01-19 17:12:16 +0000

    Refreshed html

--- refresh.sh
+++ refresh.sh
@@ -3,5 +3,5 @@ SOURCE_DIR=../farmline
 [[ ! -d $SOURCE_DIR ]] && echo "$SOURCE_DIR was not found" && exit 1
 rm -rf common
 cp -va $SOURCE_DIR/common .
-cp -vp $SOURCE_DIR/web/{*.dart,*.js} web
+cp -vp $SOURCE_DIR/web/{*.html,*.css,*.dart,*.js} web
 cp -vp $SOURCE_DIR/packages/browser/dart.js web/packages/browser
