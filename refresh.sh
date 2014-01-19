17f5b88085e573fe8a5d5e1fe57116a08b74cff5
João Pinto <lamego.pinto@gmail.com>
2014-01-19 16:55:47 +0000

    Moved dart.js to the proper place

--- refresh.sh
+++ refresh.sh
@@ -2,4 +2,4 @@
 SOURCE_DIR=../farmline
 [[ ! -d $SOURCE_DIR ]] && echo "$SOURCE_DIR was not found" && exit 1
 cp -vp $SOURCE_DIR/web/{*.dart,*.js} web
-cp -vp $SOURCE_DIR/packages/browser/dart.js web
+cp -vp $SOURCE_DIR/packages/browser/dart.js web/packages/browser
