5cad807427a28c66ce021889d63c8e96a2ba4416
João Pinto <lamego.pinto@gmail.com>
2014-01-19 17:07:25 +0000

    Added loading page

--- web/farmline.dart
+++ web/farmline.dart
@@ -9,15 +9,45 @@ part 'src/products.dart';
 
 Stage stage;
 
+Bitmap loadingBitmap;
+Tween loadingBitmapTween;
+TextField loadingTextField;
+
 void main() {
   // setup the Stage and RenderLoop
   var canvas = html.querySelector('#stage');
   stage = new Stage('myStage', canvas);
   var renderLoop = new RenderLoop();
   renderLoop.addStage(stage);
-  var juggler = renderLoop.juggler;
+  
+  //BitmapData.defaultLoadOptions.webp = true;
 
-  loadResources();
+  BitmapData.load("../common/images/Loading.png").then((bitmapData) {
+
+    loadingBitmap = new Bitmap(bitmapData);
+    loadingBitmap.pivotX = 20;
+    loadingBitmap.pivotY = 20;
+    loadingBitmap.x = 400;
+    loadingBitmap.y = 270;
+    stage.addChild(loadingBitmap);
+
+    loadingTextField = new TextField();
+    loadingTextField.defaultTextFormat = new TextFormat("Arial", 20, 0xA0A0A0, bold:true);;
+    loadingTextField.width = 240;
+    loadingTextField.height = 40;
+    loadingTextField.text = "... loading ...";
+    loadingTextField.x = 400 - loadingTextField.textWidth / 2;
+    loadingTextField.y = 320;
+    loadingTextField.mouseEnabled = false;
+    stage.addChild(loadingTextField);
+
+    loadingBitmapTween = new Tween(loadingBitmap, 100, TransitionFunction.linear);
+    loadingBitmapTween.animate.rotation.to(100.0 * 2.0 * math.PI);
+    stage.juggler.add(loadingBitmapTween);
+
+    loadResources();
+  });
+  
 }
 
 void loadResources() {
