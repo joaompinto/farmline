a5f265fe07a4d235bc45e6c337438fab219075ec
João Pinto <lamego.pinto@gmail.com>
2014-04-13 15:38:33 +0100

    Remove unneeded js update from refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -4,6 +4,6 @@ SOURCE_DIR=../farmline
 [[ ! -d $SOURCE_DIR ]] && echo "$SOURCE_DIR was not found" && exit 1
 rm -rf common
 cp -va $SOURCE_DIR/common .
-cp -vp $SOURCE_DIR/web/{*.html,*.css,*.dart,*.js} web
+cp -vp $SOURCE_DIR/web/{*.html,*.css,*.dart} web
 cp -vp $SOURCE_DIR/packages/browser/dart.js web/packages/browser
 /opt/dart/dart-sdk/bin/dart2js -o web/farmline.dart.js ../farmline/web/farmline.dart -m
